37fd424adc0b91e030f0044ad058ffe313bf6ea5
xEYKx <leemajiors@gmail.com>
2024-05-21 21:18:39 -0400

    Mern-Blog

---
+++ mernblog/client/src/App.js
@@ -0,0 +1,53 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <main>
+      <header>
+       <a href="" className="logo">MyBlog</a>
+       <nav>
+        <a href="">login</a>
+        <a href="">Register</a>
+       </nav>
+      </header>
+      <div className="post">
+        <div className="image">
+          <img src="https://techcrunch.com/wp-content/uploads/2022/12/lawnmower-Large.jpeg?resize=680,417" alt="image" />
+        </div>  
+          <div className="text">
+            <h2>EcoFlow teases full-house battery backup coming later this year</h2>
+            <p>Today at its special launch event, home backup power giant EcoFlow launched a flurry of new products, including a “Whole-Home Backup Power Solution.”</p>
+          </div>
+        </div>
+
+        <div className="post">
+        <div className="image">
+          <img src="https://techcrunch.com/wp-content/uploads/2022/12/lawnmower-Large.jpeg?resize=680,417" alt="image" />
+        </div>  
+          <div className="text">
+            <h2>EcoFlow teases full-house battery backup coming later this year</h2>
+            <p>Today at its special launch event, home backup power giant EcoFlow launched a flurry of new products, including a “Whole-Home Backup Power Solution.”</p>
+          </div>
+        </div>
+    
+        <div className="post">
+        <div className="image">
+          <img src="https://techcrunch.com/wp-content/uploads/2022/12/lawnmower-Large.jpeg?resize=680,417" alt="image" />
+        </div>  
+          <div className="text">
+            <h2>EcoFlow teases full-house battery backup coming later this year</h2>
+            <p>Today at its special launch event, home backup power giant EcoFlow launched a flurry of new products, including a “Whole-Home Backup Power Solution.”</p>
+          </div>
+        </div>
+
+      
+
+ 
+      
+    </main>
+
+  );
+}
+
+export default App;
